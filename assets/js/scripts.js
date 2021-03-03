aeb1d0d6dfcc18831d6135353c86b1bb89826336
Kyle Young <kyleyoung.9210@gmail.com>
2021-03-03 21:00:16 +0000

    putting functions, methods, and variables to be used within searchEvent into one function.  Also pushing searched cities into an array

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,5 @@
 let search = $(".search");
+
 let searchBtn = $(".searchBtn");
 let citiesList = $(".cities");
 let cityLiEl = $("<li>").addClass("list-group-item");
@@ -13,34 +14,48 @@ function createCityList() {
   cityBtn.text(search.val());
 }
 
+// putting functions, methods, and variables to be used within searchEvent into one function
+function inputFunc() {
+  event.preventDefault();
+  let searchVal = search.val();
+  createCityList();
+  citySearch.push(searchVal);
+  setStorage();
+  getWeather(searchVal);
+  console.log(citySearch);
+  search.val("");
+}
+
 // function to be called on later to implement a click event on saveBtn
-function searchBtnClick() {
+function searchEvent() {
   // adding a click event to searchBtn
   searchBtn.click(function (event) {
-    // preventing default action
-    event.preventDefault();
-    createCityList();
-    setStorage();
-    console.log(search.val());
+    inputFunc();
   });
 
   // adding a keypress event on the search input
   $(search).keypress(function (event) {
     let keyCode = event.keyCode;
     if (keyCode === 13) {
-      event.preventDefault();
-      setStorage();
-      console.log(search.val());
+      inputFunc();
     }
   });
 }
 
 function setStorage() {
-  // defining a variable to be used as the key value
-  let city = $(search).val();
-
-  // setting items into localStorage using city defined above to be a stringified value
-  localStorage.setItem("City", JSON.stringify(city));
+  // setting array cities into localStorage above to be a stringified value
+  localStorage.setItem("city", JSON.stringify(citySearch));
 }
 
-searchBtnClick();
+function getWeather(city) {
+  let apiKey = "6d2d2840d31a11364a7bbe1cba528b23";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data, city);
+    });
+  });
+}
+
+searchEvent();
