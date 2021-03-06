c4952b1d916571bc2f30c293357a616ef4a37996
Kyle Young <kyleyoung.9210@gmail.com>
2021-03-06 15:00:56 +0000

    commenting js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,19 @@
+// Setting global variables
 let search = $(".search");
-
 let searchBtn = $(".searchBtn");
 let cityBtn;
 let citiesList = $(".cities");
 let weatherContainer = $("#weather-container");
-let cities = [];
-
 let forecastContainer = $("#forecastContainer");
 let fiveDayForecast = $("#forecast");
 
-// ********** Search Functions **********
+// creating an empty array to be used below
+let cities = [];
 
+// ***************** Search Functions *****************
+// adding a click listener to searchBtn
 searchBtn.click(function (event) {
+  // if search.val() is not empty, then continue...
   if (search.val() !== "") {
     inputFunc();
   } else {
@@ -21,8 +23,11 @@ searchBtn.click(function (event) {
 
 // adding a keypress event on the search input for the 'enter' key
 $(search).keypress(function (event) {
+  // setting the key code of the button pressed to a variable
   let keyCode = event.keyCode;
+  // if keyCode strictly equals 13 (enter)...
   if (keyCode === 13) {
+    // if search.val() is not empty, then continue...
     if (search.val() !== "") {
       inputFunc();
     } else {
@@ -33,76 +38,96 @@ $(search).keypress(function (event) {
 
 // putting functions, methods, and variables to be used within searchEvent into one function
 function inputFunc() {
+  // preventing default actions of event
   event.preventDefault();
+  // removing class hidden from elements to display them
   citiesList.removeClass("hidden");
   weatherContainer.removeClass("hidden");
+  // pushing the search value into the cities array declared above
   cities.push(search.val());
+  // setting localStorage
   setStorage();
+  // create city serach btns
   createCityList(search.val());
+  // getting current and forcasted weather
   getWeather(search.val());
+  // clearing the input
   search.val("");
 }
 
+// creating a function to list search values as buttons
 function createCityList(lastCity) {
-  // console.log(cities);
+  // creating a button element
   cityBtn = $("<button>").addClass(
     "cityBtn btn btn-outline-secondary d-flex w-100"
   );
   cityBtn.text(lastCity);
   cityBtn.attr("data-city", lastCity);
+  // putting the last button made before the previous buttons
   citiesList.prepend(cityBtn);
 
+  // adding a click event on cityBtns created above
   cityBtn.click(function (event) {
+    // accessing the data attribute and getting weather
     getWeather(event.target.getAttribute("data-city"));
   });
 }
 
-// ********** localStorage **********
+// ***************** localStorage *****************
 
+// setting localStorage
 function setStorage() {
-  // setting array cities into localStorage above to be a stringified value
+  // setting cities array into localStorage above to be a stringified value
   localStorage.setItem("cities", JSON.stringify(cities));
 }
 
-// ********** API **********
+// ***************** API *****************
 
+// using a function to fetch openweathermap api to get current weather conditions
 function getWeather(city) {
+  // setting api key to a variable
   let apiKey = "6d2d2840d31a11364a7bbe1cba528b23";
+  // setting api url to a variable
   let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 
+  // fetching apiUrl
   fetch(apiUrl).then(function (response) {
-    // console.log(response);
     response.json().then(function (data) {
+      // getting current weather conditions and displaying to page
       createCurrentConditions(data, city);
     });
   });
 }
 
+// using a function to fetch openweathermap api to get current uv index
 function getUvi(lat, lon) {
   let apiKey = "6d2d2840d31a11364a7bbe1cba528b23";
   let uviUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
   fetch(uviUrl).then(function (response) {
     response.json().then(function (data) {
+      // creating current uv index and displaying to page
       createCurrentUvi(data);
     });
   });
 }
 
+// using a function to fetch openweathermap api to get forecasted weather
 function getForecast(lat, lon) {
   let apiKey = "6d2d2840d31a11364a7bbe1cba528b23";
   let forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,current,alerts&units=imperial&appid=${apiKey}`;
-  console.log(forecastUrl);
 
   fetch(forecastUrl).then(function (response) {
     response.json().then(function (data) {
+      // creating forecast and displaying to page
       createForecast(data);
     });
   });
 }
 
-// ********** Current Weather Conditions **********
+// ***************** Current Weather Conditions *****************
 
+// using a function to create current weather conditions elements
 function createCurrentConditions(conditions, city) {
   // Creating
   let currentCity = $("#currentCity");
@@ -127,17 +152,20 @@ function createCurrentConditions(conditions, city) {
   let currentWind = $("#currentWind");
   currentWind.text(conditions.list[0].wind.speed);
 
+  // setting lat and lon to be passed through getUvi and getForecast functions below
   let lat = conditions.city.coord.lat;
   let lon = conditions.city.coord.lon;
   getUvi(lat, lon);
   getForecast(lat, lon);
 }
 
+// using a function to create current uv index elements
 function createCurrentUvi(uvi) {
   let currentUVI = $("#currentUVI").addClass("text-white p-1");
   let uviIndex = uvi.current.uvi;
   currentUVI.text(uviIndex);
 
+  // setting background color of uv index depending on the severity
   if (uviIndex <= 2) {
     currentUVI.addClass("bg-success");
   } else if (uviIndex > 2 && uviIndex <= 7) {
@@ -147,9 +175,13 @@ function createCurrentUvi(uvi) {
   }
 }
 
+// using a function to create current forecasted weather elements
 function createForecast(forecast) {
+  // clearing any previous elements created
   fiveDayForecast.text("");
+
   let dailyForecast = forecast.daily;
+  // looping through forecasted days from api and stopping at the 5th day
   for (let i = 1; i < 6; i++) {
     let nextDay = dailyForecast[i];
     let forcastedDay = moment(nextDay.dt * 1000).format("M/D/YYYY");
